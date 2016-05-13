2ba25c1acdaa8800894a5447dcf462b68ab9c04b
LHS <LHS@LHSui-MacBook-Air.local>
2016-05-13 21:40:38 +0900

    .

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '143.248.106.223';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Debater Project\n');
 });
 
 server.listen(port, hostname, () => {
